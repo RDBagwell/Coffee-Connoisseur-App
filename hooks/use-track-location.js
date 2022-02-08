321e38b9d3331ab6b3a631da89fc5295511f6ec8
RDBagwell <erbagwell@yahoo.com>
2022-02-07 21:06:37 -0700

    Add Hooks

--- hooks/use-track-location.js
+++ hooks/use-track-location.js
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export default function useTrackLocation(){
-    return {}
+
+    const [locationErrorMsg, setLocationErrorMsg] = useState('');
+    const [latLong, setLatLong] = useState('');
+    const [isFindingLocation, setIsFindingLocation] = useState(false);
+    
+    const success = (postion)=>{
+        const latitude = postion.coords.latitude;
+        const longitude = postion.coords.longitude;
+        setLatLong(`${latitude},${longitude}`);
+        setLocationErrorMsg('');
+        setIsFindingLocation(false);
+    };
+    
+    const error = ()=>{
+        setIsFindingLocation(false);
+        setLocationErrorMsg("Can not find your location.");
+        setIsFindingLocation(false);
+    }
+
+    const hanleTrackLocation = ()=>{
+        setIsFindingLocation(true);
+        if(!navigator.geolocation){
+            setIsFindingLocation(false);
+            setLocationErrorMsg("Geolocation is not supported by your browser");
+        } else{
+            // status.texContent = "Locating...";
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+    }
+    return {
+        latLong,
+        locationErrorMsg,
+        isFindingLocation,
+        hanleTrackLocation
+    }
 }
