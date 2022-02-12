2c5bbefd07aa1b6fad7d8bd06ea30449c93cb352
Robert B <robertb@lifesciencepublishers.com>
2022-02-11 17:02:16 -0700

    Add Create With  AirTable

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,52 @@
 const Airtable = require('airtable');
 const base = new Airtable({apiKey: process.env.AIR_TABLE_API_KEY}).base(process.env.AIR_TABLE_API_BASE);
 
-const table = base('coffee-stores');
+
+
+export default async function createCoffeeStore(req, res){
+
+    const table = base('coffee-stores');
+
+    const { id, name, locality, address, votes, imageURL } = req.body;
+
+    try {
+        if (req.method === 'POST') {
+            // Find a record
+            const findCoffeeStoreRecords = await table.select({
+                filterByFormula: `id="${id}"`
+            }).firstPage();
+    
+            if(findCoffeeStoreRecords.length !== 0){
+                const records = findCoffeeStoreRecords.map((record)=>{
+                    return {
+                        ...record.fields
+                    }
+                })
+                res.status(200).json(records);
+            } else {
+                const createRecords = await table.create([
+                    {
+                        "fields": {
+                            id,
+                            name,
+                            locality,
+                            address,
+                            votes,
+                            imageURL
+                        }
+                    }
+                ]);
+                res.status(200).json({succsse: "Created Record", records: createRecords});
+            }
+    
+            // res.status(200).json(req.body);
+        } else {
+            res.status(200).json({succsse: "Bad"});
+        }
+        
+    } catch (error) {
+        res.status(500).json({error: `Can't find Record... ${error}`})
+    }
+
+
+}
